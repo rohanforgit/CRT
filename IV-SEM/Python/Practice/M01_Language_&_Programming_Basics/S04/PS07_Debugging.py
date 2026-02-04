58e59985949942a8d2b619341ffb33c043dc7a64
rohanforgit <2401100cse0275@cse.suh.edu.in>
2026-02-04 15:41:04 +0530

    Asignment

--- IV-SEM/Python/Practice/M01_Language_&_Programming_Basics/S04/PS07_Debugging.py
+++ IV-SEM/Python/Practice/M01_Language_&_Programming_Basics/S04/PS07_Debugging.py
@@ -1,9 +1,30 @@
-# random value function
+# # random value function
+# import random
+
+# li = [1,2,3]
+# # i = random.randint(0,3) because 3 is not an index in the list
+# i = random.randint(0,2)
+
+
+# print(i,li[i])
+
+#p i ==>variable values 
+#n==> next line 
+#c==>continue execution 
+#q==>quit 
+
+
+
+
 import random
-
-li = [1,2,3]
-# i = random.randint(0,3) because 3 is not an index in the list
-i = random.randint(0,2)
-
-
-print(i,li[i])
+import math
+import pdb
+arr = [1,2,3,4,5]
+i = random.randint(0,5)
+print(i)
+print("hello world")
+print("helo world")
+print(arr[i])
+print("hello world")
+print(dir(math))
+print(help("math"))
