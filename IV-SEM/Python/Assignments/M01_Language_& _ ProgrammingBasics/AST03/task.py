038fba3dbec920221f92c9d63c0c4e67dbc96450
rohanforgit <notrohan7@gmail.com>
2026-03-06 10:30:53 +0530

    final push

--- IV-SEM/Python/Assignments/M01_Language_& _ ProgrammingBasics/AST03/task.py
+++ IV-SEM/Python/Assignments/M01_Language_& _ ProgrammingBasics/AST03/task.py
@@ -7,10 +7,9 @@ def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
     else:
         status = "fail"
 
-    return f"Average grade: {round(average, 2)}, Status: {status}"
+    return f"Average grade: {average}, Status: {status}"
 
 if __name__ == '__main__':
     name = input()
     n1,n2,n3 = list(map(int,input().split()))
     print(Student_Grade_System(name,n1,n2,n3))
-    #done
